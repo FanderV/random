a5c8d8e129f61d9e3af1d70fa4497cd9bf095b6a
FanderV <113282846+FanderV@users.noreply.github.com>
2022-10-05 16:52:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+spisok=[]
+
+plus = 0
+minus = 0
+i = 0
+
+while i < 15:
+    i = i + 1
+    z = random.randint(-100, 100)
+    spisok.append(z)
+    if z == 0:
+        while z == 0:
+            z = random.randint(-100, 100)
+    if z > 0:
+        plus = plus + z
+    else:
+        minus = minus + z
+
+print("15 случайных чисел:", spisok)
+print("Сумма положительных:", plus)
+print("Сумма отрицательных:", minus)
+
+
